99fe17b9d677f8e0d9de2191f58e0f6aa541a0a4
Tim White <timma.j.white@gmail.com>
2018-12-19 16:40:06 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/bin/node
+const spawn = require( 'child_process' ).spawnSync;
+const fs = require( 'fs' );
+const program = require( 'commander' );
+const colors = require( 'colors' );
+
+function list ( val ){
+  return val.split( ',' );
+}
+
+program
+  .parse( process.argv );
