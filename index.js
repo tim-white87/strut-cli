da7f03508f240d99146e2487a6300f5698189e04
Tim White <timma.j.white@gmail.com>
2018-12-19 18:52:55 -0700

    added switch case for ags

--- index.js
+++ index.js
@@ -4,18 +4,31 @@
 const program = require('commander');
 const colors = require('colors');
 
-function list(val) {
-  return val.split(',');
-}
+console.log(colors.blue('Welcome to Strut!'));
 
 program
   .version('0.0.1', '-v, --version')
-  .arguments('<cmd>') // 'initialize an AWS Project'
-  .action(cmd => {
-    console.log(cmd);
+  .arguments('<cmd> [value]') // 'initialize an AWS Project'
+  .action((cmd, value) => {
+    switch (cmd) {
+      case 'init':
+        console.log('init', value);
+        break;
+      case 'provision':
+        console.log('provision');
+        break;
+      default:
+        break;
+    }
   })
-  // .command('provision', 'Provisions the project\'s accounts with the defined infrastructure')
+  .on('--help', function () {
+    console.log('');
+    console.log('Commands:');
+    console.log('  init [name]    Inits a Project as an OU in AWS');
+    console.log('  provision      Provisions the accounts in the project from the infrastructure.json');
+  })
   .parse(process.argv);
 
-console.log(colors.blue('Welcome to Strut!'));
-
+//   function list(val) {
+//   return val.split(',');
+// }
