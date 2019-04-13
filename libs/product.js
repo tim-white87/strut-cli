392f7d19171abb831ca2dd1fd175d2a1d33001f9
Timothy White <timma.j.white@gmail.com>
2019-04-13 12:15:51 -0600

    added some user feedback added application model

--- libs/product.js
+++ libs/product.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const colors = require('colors');
 
 exports.Product = class Product {
   get productModel() {
@@ -10,13 +11,29 @@ exports.Product = class Product {
     };
   };
 
+  get applicationModel () {
+    return {
+      name: null,
+      repository: {
+        type: null,
+        url: null
+      },
+      path: null, // optionally point at local folder for app
+      install: [], // install commands
+      build: [], // build commands
+      artifacts: [] // artifact paths
+    };
+  };
+
   async create (name) {
+    console.log(colors.yellow(`Creating product: ${colors.gray(name)}`));
     this.name = name || 'myproduct';
     this.dir = `./${name}`;
     if (!fs.existsSync(this.dir)) {
       fs.mkdirSync(this.dir);
     }
     await this.updateProductFile({ ...this.productModel, name });
+    console.log(colors.green('DONE!'));
   }
 
   async updateProductFile (data) {
