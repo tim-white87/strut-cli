2dfb22ea6a35df554f77c96380f992ac739b3098
Timothy White <timma.j.white@gmail.com>
2019-05-06 19:24:03 -0600

    setup spawn as async and await

--- libs/utils.js
+++ libs/utils.js
@@ -1,10 +1,10 @@
 const { spawn } = require('child_process');
 const colors = require('colors');
 
-function run (command, args = [], options) {
+async function run (command, args = [], options) {
   console.log(colors.gray('Path: ', colors.green(options.cwd || process.cwd())));
   console.log(colors.gray('Command: ', colors.green(command)));
-  const child = spawn(command, args, {
+  const child = await spawn(command, args, {
     stdio: 'inherit',
     shell: true,
     ...options
