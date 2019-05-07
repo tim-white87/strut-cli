8a2f0b9ae62b62f3470791053f1e79883e0cabd1
Timothy White <timma.j.white@gmail.com>
2019-05-07 11:04:19 -0600

    adding in aws sdk

--- libs/providers/awsModel.js
+++ libs/providers/awsModel.js
@@ -1,9 +1,11 @@
-
+const CloudFormation = require('aws-sdk/clients/cloudformation');
 const { ProviderModel } = require('./baseProviderModel');
+const cloudformation = new CloudFormation();
 
 class AwsModel extends ProviderModel {
   async load() {
     console.log('providing aws');
+    console.log(this.application);
   }
 };
 
--- libs/utils.js
+++ libs/utils.js
@@ -4,26 +4,10 @@ const colors = require('colors');
 async function run (command, args = [], options) {
   console.log(colors.gray('Path: ', colors.green(options.cwd || process.cwd())));
   console.log(colors.gray('Command: ', colors.green(command)));
-  const child = await spawn(command, args, {
+  await spawn(command, args, {
     stdio: 'inherit',
     shell: true,
     ...options
-  });
-
-  child.on('error', function (err) {
-    console.log(err);
-  });
-
-  child.stdout.on('data', (data) => {
-    console.log(data);
-  });
-
-  child.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-  });
-
-  child.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
   });
 }
 
