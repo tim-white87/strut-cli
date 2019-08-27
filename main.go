b7c8b63aff0e533ae4212f41c5ba6ed7be39e7ab
Timothy White <timma.j.white@gmail.com>
2019-08-26 22:37:20 -0600

    copying over strut commands

--- main.go
+++ main.go
@@ -2,8 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/urfave/cli"
 )
 
 func main() {
-	fmt.Printf("Hello strut\n")
+	color.Blue("Welcome to Strut!")
+	app := cli.NewApp()
+	app.Name = "strut"
+
+	app.Commands = []cli.Command{
+		{
+			Name:      "create",
+			Usage:     "Create a new strut product",
+			ArgsUsage: "[name]",
+			Action: func(c *cli.Context) error {
+				fmt.Println("first arg: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:      "add",
+			Usage:     "Add an <application|provider> to the product",
+			ArgsUsage: "<type> [name] [value]",
+			Action: func(c *cli.Context) error {
+				fmt.Println("first arg: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:      "run",
+			Usage:     "Runs the specified command <install|build|start> for the product applications (separated with a comma). Default will run all apps.",
+			ArgsUsage: "<cmd> [applications]",
+			Action: func(c *cli.Context) error {
+				fmt.Println("first arg: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:      "provision",
+			Usage:     "Provisions the defined infrastructure for the applications to the specified provider. Defaults to all applications deployed to all providers",
+			ArgsUsage: "[applications] [providers]",
+			Action: func(c *cli.Context) error {
+				fmt.Println("first arg: ", c.Args().First())
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
