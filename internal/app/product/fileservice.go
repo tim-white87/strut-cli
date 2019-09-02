8fff5e456c94ab03535faf987aedb584aae5e22d
Timothy White <timma.j.white@gmail.com>
2019-09-02 08:49:56 -0600

    stubbed in unit test file for fileservice

--- internal/app/product/fileservice.go
+++ internal/app/product/fileservice.go
@@ -4,16 +4,16 @@ package product
 type FileService struct{}
 
 // CreateFile Creates the product file in JSON or YAML
-func (m *FileService) CreateFile() {}
+func (fs *FileService) CreateFile() {}
 
 // ReadFile Loads the product file from the CWD
-func (m *FileService) ReadFile() {}
+func (fs *FileService) ReadFile() {}
 
 // UpdateFile Updates the product file
-func (m *FileService) UpdateFile() {}
+func (fs *FileService) UpdateFile() {}
 
 // DeleteFile Deletes the product file
-func (m *FileService) DeleteFile() {}
+func (fs *FileService) DeleteFile() {}
 
 // AddApplication Adds an application to product file
-func (m *FileService) AddApplication() {}
+func (fs *FileService) AddApplication() {}
---
+++ internal/app/product/fileservice_test.go
@@ -0,0 +1,13 @@
+package product
+
+import "testing"
+
+func TestCreate(t *testing.T) {}
+
+func TestRead(t *testing.T) {}
+
+func TestUpdate(t *testing.T) {}
+
+func TestDelete(t *testing.T) {}
+
+func TestAddApplication(t *testing.T) {}
