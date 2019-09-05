630e71a8172990279666f98652f991ba5b87fe42
Timothy White <timma.j.white@gmail.com>
2019-09-04 20:39:18 -0600

    refactored for better model

--- internal/app/product/model.go
+++ internal/app/product/model.go
@@ -1,9 +1,14 @@
 package product
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/cecotw/strut-cli/internal/pkg/file"
+	"github.com/ghodss/yaml"
 )
 
 // ProductFileName name of the product file
@@ -11,40 +16,78 @@ const ProductFileName = "strut"
 
 // Model The product model
 type Model interface {
-	writeFile(model model) (*Product, error)
-	readFile(model model) (*Product, error)
+	writeFile() ([]byte, error)
+	readFile() ([]byte, error)
+	LoadProduct() (*Product, error)
 	SaveProduct() (*Product, error)
 	AddApplication(application *Application) (*Product, error)
 }
 
 type model struct {
-	*fileService
-	*productService
 	Product  *Product
 	fileType *file.Type
 }
 
 // New product model constructor
 func New(fileType *file.Type) Model {
-	return model{
-		&fileService{},
-		&productService{},
+	return &model{
 		&Product{},
 		fileType,
 	}
 }
 
-type productService struct{}
-
-func (ps *productService) LoadProduct() (*Product, error) {
-	return nil, fmt.Errorf("error")
+func (m *model) LoadProduct() (*Product, error) {
+	data, err := m.readFile()
+	switch m.fileType {
+	case file.Types.JSON:
+		err = json.Unmarshal(data, m.Product)
+	case file.Types.YAML:
+		err = yaml.Unmarshal(data, m.Product)
+	}
+	return m.Product, err
 }
 
-func (ps *productService) SaveProduct() (*Product, error) {
+func (m *model) SaveProduct() (*Product, error) {
 	return nil, fmt.Errorf("error")
 }
 
 // AddApplication Adds an application to product and updates the file
-func (ps *productService) AddApplication(application *Application) (*Product, error) {
+func (m *model) AddApplication(application *Application) (*Product, error) {
 	return nil, fmt.Errorf("error")
+}
+
+// writeFile Writes the product file in JSON or YAML
+func (m *model) writeFile() ([]byte, error) {
+	var fileName = fmt.Sprintf("%s.%s", ProductFileName, m.fileType.Extension)
+	var data []byte
+	var err error
+	switch m.fileType {
+	case file.Types.YAML:
+		{
+			data, err = yaml.Marshal(m.Product)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	case file.Types.JSON:
+		{
+			data, err = json.MarshalIndent(m.Product, "", "  ")
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	err = ioutil.WriteFile(fileName, data, 0644)
+	return m.readFile()
+}
+
+// readFile Loads the product file from the CWD
+func (m *model) readFile() ([]byte, error) {
+	fileData, err := os.Open(fmt.Sprintf("%s.%s", ProductFileName, m.fileType.Extension))
+	defer fileData.Close()
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return ioutil.ReadAll(fileData)
 }
--- internal/app/product/model_test.go
+++ internal/app/product/model_test.go
@@ -1,8 +1,12 @@
 package product
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
+
+	"github.com/cecotw/strut-cli/internal/pkg/file"
+	"github.com/ghodss/yaml"
 )
 
 func init() {
@@ -20,4 +24,49 @@ func TestSaveProduct(t *testing.T) {
 
 func TestAddApplication(t *testing.T) {
 	t.Fatalf("Expected implementation")
+}
+
+func TestWriteFile(t *testing.T) {
+	// Arrange
+	yamlProductModel := New(file.Types.YAML).(*model)
+	jsonProductModel := New(file.Types.JSON).(*model)
+
+	// Act
+	_, yamlErr := yamlProductModel.writeFile()
+	_, jsonErr := jsonProductModel.writeFile()
+
+	// Assert
+	if _, err := os.Stat("./strut.yaml"); os.IsNotExist(err) {
+		t.Fatalf("Expected ./strut.yaml file to exist.")
+	}
+	if _, err := os.Stat("./strut.json"); os.IsNotExist(err) {
+		t.Fatalf("Expected ./strut.json file to exist.")
+	}
+	if yamlErr != nil {
+		t.Fatalf("Expected no YAML error: %s", yamlErr)
+	}
+	if jsonErr != nil {
+		t.Fatalf("Expected no JSON error: %s", jsonErr)
+	}
+}
+
+func TestReadFile(t *testing.T) {
+	// Arrange
+	yamlProductModel := New(file.Types.YAML).(*model)
+	jsonProductModel := New(file.Types.JSON).(*model)
+
+	// Act
+	yamlData, err := yamlProductModel.readFile()
+	jsonData, err := yamlProductModel.readFile()
+
+	// Assert
+	if err != nil {
+		t.Fatalf("Expected to read file: %s", err)
+	}
+	if err := yaml.Unmarshal(yamlData, yamlProductModel.Product); err != nil {
+		t.Fatalf("Expected strut.yaml file to parse to model: %s", err)
+	}
+	if err := json.Unmarshal(jsonData, jsonProductModel.Product); err != nil {
+		t.Fatalf("Expected strut.json file to parse to model: %s", err)
+	}
 }
